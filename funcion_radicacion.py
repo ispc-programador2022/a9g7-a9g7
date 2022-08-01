d5c8d81df144d3bfdb9a3c81f56aeab726c9fc90
just-anto <85323426+just-anto@users.noreply.github.com>
2022-08-01 01:20:21 -0300

    Add files via upload

---
+++ funcion_radicacion.py
@@ -0,0 +1,9 @@
+num1 = int(input("Ingrese el número a sacar la raíz cuadrada"))
+num2 = int(input("Ingrese el número de la raíz"))
+
+
+def raiz(x, y):
+    return x // y
+
+
+print("El resultado de la raíz es: ", float(raiz(num1, num2)))
