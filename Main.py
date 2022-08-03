592fe92a9869b99a612a02a613f90bcf1b62e176
CelesteCeleste18 <110560935+CelesteCeleste18@users.noreply.github.com>
2022-08-03 15:13:55 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,3 +3,9 @@ def suma (num1,num2):
 
 def resta (num1,num2):
     return (num1 - num2) 
+
+def producto (num1,num2):
+    return (num1 * num2) 
+    
+def cociente (num1,num2):
+    return (num1 / num2)   
